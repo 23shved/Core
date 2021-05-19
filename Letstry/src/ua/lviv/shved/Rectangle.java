8557bae7c49f4df1b3696d4616fe1017f9f17f99
23.shved <testshved@gmail.com>
2021-05-19 19:21:32 +0300

    app

---
+++ Letstry/src/ua/lviv/shved/Rectangle.java
@@ -0,0 +1,31 @@
+package ua.lviv.shved;
+
+public class Rectangle {
+    private int w;
+    private int h;
+    
+    Rectangle(){
+    }
+    public void setWidth(int width) {
+		this.w = width;
+	}
+    public void setHeight(int height) {
+		this.h = height;
+	}
+    Rectangle(int width, int height){
+        this.w = width;
+        this.h = height;
+    }
+     public int areaRect(){
+         return calculationOfareaRect(w,  h);
+     }
+    private int calculationOfareaRect(int width, int height){
+        return width*height;
+    }
+    public int perimeterRect(){
+        return calculationOfperimeterRect(w,  h);
+    }
+   private int calculationOfperimeterRect(int width, int height){
+       return 2*(width+height);
+   }
+}
