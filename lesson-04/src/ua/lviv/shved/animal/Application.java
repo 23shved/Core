b5d233240f6e3903aaf694fd908b57fc21c66134
23.shved <testshved@gmail.com>
2021-05-23 00:48:06 +0300

    speed change

--- lesson-04/src/ua/lviv/shved/animal/Application.java
+++ lesson-04/src/ua/lviv/shved/animal/Application.java
@@ -2,7 +2,7 @@ package ua.lviv.shved.animal;
 
 public class Application {
 	public static void main(String[] args) {
-		Animal animal = new Animal("Котик", 25.5, 7);
+		Animal animal = new Animal("Котик", 25.2, 7);
 		System.out.println("Назва тварини = " + animal.getName() + ", Швидкість тварини = " + animal.getSpeed() + " км/год, Вік тварини = " + animal.getAge() + " років");
 		
 		animal.setName("Песик");
