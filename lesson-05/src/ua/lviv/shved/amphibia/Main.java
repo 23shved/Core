31bd35814820b6f55fe6c962433bd76b2eafcebd
23shved <81774928+23shved@users.noreply.github.com>
2021-05-26 10:44:15 +0300

    Update Main.java

--- lesson-05/src/ua/lviv/shved/amphibia/Main.java
+++ lesson-05/src/ua/lviv/shved/amphibia/Main.java
@@ -3,7 +3,7 @@ package ua.lviv.shved.amphibia;
 public class Main {
 
 	public static void main(String[] args) {
-		Frog f = new Frog("Frog");
+		Amphibia f = new Frog("Frog");
 		f.eat();
 		f.sleep();
 		f.swim();
