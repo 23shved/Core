55ae7fdb847952b78f2035486d2ffd1a97ccebe0
23shved <81774928+23shved@users.noreply.github.com>
2021-05-31 22:18:08 +0300

    Delete lesson-04 directory

--- lesson-04/src/ua/lviv/shved/robot/RobotCoocker.java
+++
@@ -1,9 +0,0 @@
-package ua.lviv.shved.robot;
-
-public class RobotCoocker extends Robot{
-
-	RobotCoocker(String name, String action) {
-		super(name, action);
-	}
-
-}
--- lesson-04/src/ua/lviv/shved/robot/RobotDancer.java
+++
@@ -1,9 +0,0 @@
-package ua.lviv.shved.robot;
-
-public class RobotDancer extends Robot{
-
-	RobotDancer(String name, String action) {
-		super(name, action);
-	}
-
-}
